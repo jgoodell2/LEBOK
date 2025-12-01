d07da2c6b7ae816ae35e21e2798196045c7c1fa1
Nathaniel Rudenberg <rnatou12@gmail.com>
2025-11-30 19:16:38 -0500

    Finish navigation buttons, add update logic

--- populator/populate_wiki.py
+++ populator/populate_wiki.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import enum
 import requests
 import json
 import re
@@ -121,7 +122,7 @@ def get_page(path: str):
         print(f"Error: Could not find query file for getting a page: {e.filename}")
 
 
-def update_page(id: int, content: str):
+def update_page(id: int, path: str, title: str, content: str) -> bool:
     """
     Update a page's content given its id.
     """
@@ -135,12 +136,25 @@ def update_page(id: int, content: str):
 
             variables = {
                 "id": id,
+                "path": path,
+                "title": title,
+                "description": title,
+                "locale": LOCALE,
                 "content": content,
+                "editor": "code",
             }
 
-            return run_graphql_query(update_page_m, variables)
+            response = run_graphql_query(update_page_m, variables)
+            return (
+                response.get("data", {})
+                .get("pages", {})
+                .get("update", {})
+                .get("responseResult", {})
+                .get("succeeded", False)
+            )
     except FileNotFoundError as e:
         print(f"Error: Could not find mutation file for updating a page: {e.filename}")
+        return False
 
 
 def delete_page(id: int) -> bool:
@@ -285,14 +299,42 @@ def create_wiki_pages(
             # Append the list of links to the parent page content
             content += f"\n<hr>\n<h2>Subsections:</h2>\n<ul>\n{links_list}\n</ul>"
 
+        # Inject prev/next buttons
+        if nav_map and full_page_path in nav_map:
+            content += nav_map[full_page_path]
+
+        # Check if page exists
+        page_info = get_page(full_page_path)
+        page_exists = page_info and "id" in page_info
+        page_id = int(page_info["id"]) if page_exists else None
+
+        # Update existing pages if update flag is set
+        if do_update:
+            print(f"Updating page '{full_page_path}' (ID: {page_id})...", end="")
+
+            if update_page(page_id, full_page_path, title, content):
+                print("Done.")
+            else:
+                print("Failed.")
+
+            # If successful, recurse and then continue (skip page creation) to the next sibling page
+            if node["children"]:
+                create_wiki_pages(
+                    node["children"],
+                    create_page_mutation,
+                    do_replace,
+                    do_update,
+                    nav_map,
+                )
+            continue
+
         # Delete existing page if replacement flag is set
         if do_replace:
-            print(f"Deleting page '{full_page_path}'...", end="")
+            print(f"Deleting page '{full_page_path}' (ID: {page_id})...", end="")
             page_info = get_page(full_page_path)
 
             if page_info and "id" in page_info:
                 page_id = page_info["id"]
-                # print(f"Deleting page '{full_page_path}' (ID: {page_id})...", end="")
 
                 if delete_page(page_id):
                     print("Done.")
@@ -313,7 +355,9 @@ def create_wiki_pages(
         run_graphql_query(create_page_mutation, variables)
 
         if node["children"]:
-            create_wiki_pages(node["children"], create_page_mutation, do_replace)
+            create_wiki_pages(
+                node["children"], create_page_mutation, do_replace, do_update, nav_map
+            )
 
 
 def create_slug(text):
@@ -365,6 +409,12 @@ def main():
         help="Delete a page (if it exists) before creating it on the wiki",
     )
 
+    group.add_argument(
+        "--update",
+        action="store_true",
+        help="Updates pages (if existing) to preserve history instead of deleting",
+    )
+
     args = parser.parse_args()
 
     if args.purge or args.purge_only:
@@ -385,12 +435,11 @@ def main():
         if args.purge_only:
             sys.exit(0)
 
-    do_replace = False
-    if args.replace:
-        do_replace = True
+    do_replace = args.replace
+    do_update = args.update
 
     if not args.filepath:
-        print("Error: filepath option required unless using --nuke-only")
+        print("Error: filepath option required unless using --purge-only")
         sys.exit(1)
 
     try:
@@ -405,8 +454,13 @@ def main():
     document_tree = parse_book.get_sections(args.filepath)
     nav_items = create_navlinks(document_tree)
 
+    # Build navigation map
+    nav_map = build_nav_map(document_tree)
+
     print("Populating wiki...")
-    create_wiki_pages(document_tree, create_page_mutation, do_replace)
+    create_wiki_pages(
+        document_tree, create_page_mutation, do_replace, do_update, nav_map
+    )
 
     print("Making navlinks...")
     create_navigation(nav_items)
